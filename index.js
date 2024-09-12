11413d3528d88340476895c3f8c2f70ec212cd4e
usama9293 <usamaarshad9293@gmail.com>
2024-09-12 14:49:31 +0500

    Refactor code to add namespace for socket.io connection

--- index.js
+++ index.js
@@ -5,15 +5,14 @@ import path from "path";
 import ejsMate from "ejs-mate";
 
 const app = express();
-const server = createServer(app); // Creating an HTTP server for socket.io
-const io = new Server(server); // Initialize socket.io on the server
+const server = createServer(app); 
+const io = new Server(server); 
 
-// Setup middleware
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Set EJS and EJS-Mate
+
 app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(path.resolve(), "views"));
@@ -23,18 +22,22 @@ app.get("/", (req, res) => {
   res.render("body");
 });
 
+
+//namespace
+const tech=io.of("/tech")
+
 // Socket.io Connection
-io.on("connection", (socket) => {
+tech.on("connection", (socket) => {
   console.log("connected");
 
   socket.on("message", (msg) => {
     console.log(msg);
-    io.emit("message", msg);
+    tech.emit("message", msg);
   });
 
   socket.on("disconnect", () => {
     console.log("A user disconnected");
-    io.emit("message", "A user disconnected");
+    tech.emit("message", "A user disconnected");
   });
 });
 
